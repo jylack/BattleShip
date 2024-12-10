1a3763bc626e68adad28aca80488040e0b4c0689
jylack <jyl011202@gmail.com>
2024-12-10 16:50:08 +0900

    class seting
    asdf

--- GameManager.cs
+++ GameManager.cs
@@ -8,6 +8,22 @@ namespace BattleShip
 {
     internal class GameManager
     {
+        //플레이어,cpu 생성 , 시작화면 생성
+        public void InitGame()
+        {
 
+        }
+
+        //턴 반복되는동안 수행 여기서 좌표입력받고 cpu가 공격하고 실행함.
+        public void UpdateGame()
+        {
+
+        }
+
+        //게임도중 조건이되어 끝났을떄 게임 승패 표기
+        public void EndGame()
+        {
+
+        }
     }
 }
--- Player.cs
+++ Player.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace BattleShip
 {
+    enum Skills
+    {
+        start,end
+    }
+
     internal class Player
     {
+        string _name;
+        Skills _skill;
+
+        //새로시작하거나 처음 시작할때 플레이어 세팅 초기화.
+        public void InitPlayer()
+        {
+
+        }
+
+        //초기에 배들 배열에 배들넣기.
+        public void InitShip()
+        {
+
+        }
+
+        //배들 위치배정
+        public void PlaceShip()
+        {
+
+        }
+        
+        //랜덤으로 배들 놓기
+        public void PlaceRandomShips()
+        {
+
+        }
+
     }
 }
