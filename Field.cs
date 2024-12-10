48a85d8454f6b857bb8bb5007a860b60d20cc3d1
dduckchul <dduckchul@naver.com>
2024-12-10 16:49:31 +0900

    [feat] Field, Ship 클래스 및 변수 선언

---
+++ Field.cs
@@ -0,0 +1,14 @@
+namespace BattleShip
+{
+    public class Field
+    {
+        private bool[,] _sea;
+
+        public bool[,] Sea
+        {
+            get;
+            set;
+        }
+        
+    }
+}
